5b529a50be9a6697aba2c94f95a82b3e9ba75093
Johan Hernandez <j.h25@hotmail.com>
2023-06-16 18:07:25 -0500

    Cambio endpoints MV

--- src/main/java/Eventtum/Modelo/Usuarios.java
+++ src/main/java/Eventtum/Modelo/Usuarios.java
@@ -17,7 +17,7 @@ public class Usuarios implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="idusuario")
-    private Integer id;
+    private Integer idusuario;
     
     @Column(name="usunombre")
     private String name;
