848f71d531c3a5c8464d62ddc5174b8790e300b9
Johan Hernandez <j.h25@hotmail.com>
2023-06-16 15:19:24 -0500

    Cambio endpoints MV

--- src/main/resources/static/js/Funciones.js
+++ src/main/resources/static/js/Funciones.js
@@ -162,8 +162,9 @@ function Autenticacion(email, password) {
                 document.getElementById("welcome").removeAttribute("hidden");
                 document.getElementById("welcome").innerHTML="Bienvenido "+respuesta.name+". Ahora puede reservar locaciones! ";
                 mostrarnavbar();
-                setTimeout(function(){window.location.href = "reserva.html";}, 2500);
-                setTimeout(function(){window.location.href = "reserva.html";},1000)
+                setTimeout(function(){window.location.href = "reserva.html".window.location.reload();}, 2000);
+               
+            }
 
         }
     });
